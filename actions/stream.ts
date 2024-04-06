94851bd8b65142ccf9f746e35c15393d898f0b8b
jazzdestructor <jazzdestructor@gmail.com>
2024-04-06 19:56:06 +0530

    Added Chat Settings

---
+++ actions/stream.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { getSelf } from "@/lib/auth-service";
+import { db } from "@/lib/db";
+import { getStreamByUserId } from "@/lib/stream-service";
+import { Stream } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
+export const updateStream = async (values: Partial<Stream>) => {
+  try {
+    const self = await getSelf();
+    const selfStream = await getStreamByUserId(self.id);
+    if (!selfStream) {
+      throw new Error("Stream not found");
+    }
+    const validData = {
+      name: values.name,
+      isChatEnabled: values.isChatEnabled,
+      isChatFollowersOnly: values.isChatFollowersOnly,
+      isChatDelayed: values.isChatDelayed,
+    };
+
+    const stream = await db.stream.update({
+      where: {
+        id: selfStream.id,
+      },
+      data: { ...validData },
+    });
+
+    revalidatePath(`/u/${self.username}/chat`);
+    revalidatePath(`/u/${self.username}`);
+    revalidatePath(`/${self.username}`);
+
+    return stream;
+  } catch (error: any) {
+    throw new Error(error.message || "Internal Server Error");
+  }
+};
---
+++ lib/stream-service.ts
@@ -0,0 +1,10 @@
+import { db } from "./db";
+
+export const getStreamByUserId = async (userId: string) => {
+  const stream = await db.stream.findUnique({
+    where: {
+      userId,
+    },
+  });
+  return stream;
+};

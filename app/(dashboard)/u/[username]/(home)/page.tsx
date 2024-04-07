773be26e18fdacff5dc4c54ba17f02c1452e98a8
jazzdestructor <jazzdestructor@gmail.com>
2024-04-07 21:55:30 +0530

    Added video component setup

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import { currentUser } from "@clerk/nextjs";
 
-type Props = {};
+import { getUserByUsername } from "@/lib/user-service";
+import { StreamPlayer } from "@/components/stream-player";
 
-const CreatorPage = (props: Props) => {
-  return <div>CreatorPage</div>;
+type CreatorPageProps = {
+  params: {
+    username: string;
+  };
+};
+
+const CreatorPage = async ({ params }: CreatorPageProps) => {
+  const externalUser = await currentUser();
+  const user = await getUserByUsername(params.username);
+
+  if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
+    throw new Error("Unauthorized");
+  }
+
+  return (
+    <div className="h-full">
+      <StreamPlayer user={user} stream={user.stream} isFollowing />
+    </div>
+  );
 };
 
 export default CreatorPage;
